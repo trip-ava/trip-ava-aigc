d3a4bddf43ffd1cb70e3705c6a8a98718873e971
kevinten <596823919@qq.com>
2023-10-14 11:23:31 +0800

    feat: 添加了本地文件上传s3的功能；实现了mp3音频转文字的功能；添加了初版的gpt提示语

---
+++ src/main/java/group/rxcloud/ava/aigc/ai/aws/AwsTranscribeVoiceService.java
@@ -0,0 +1,142 @@
+package group.rxcloud.ava.aigc.ai.aws;
+
+import com.google.gson.Gson;
+import group.rxcloud.ava.aigc.config.AwsConfig;
+import group.rxcloud.ava.aigc.utils.AwsS3Utils;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.services.transcribe.TranscribeClient;
+import software.amazon.awssdk.services.transcribe.model.*;
+
+import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * AWS Transcribe 语音文本翻译服务
+ */
+@Slf4j
+@Service
+public class AwsTranscribeVoiceService {
+
+    public static final String ZH = "zh-CN";
+
+    private static Gson GSON = new Gson();
+
+    private TranscribeClient transcribeClient;
+
+    @Autowired
+    private AwsS3Utils awsS3Utils;
+
+    @PostConstruct
+    public void init() {
+        // 创建Amazon Transcribe V2
+        transcribeClient = TranscribeClient.builder()
+                .region(AwsConfig.regionsV2)
+                .credentialsProvider(() -> AwsBasicCredentials.create(AwsConfig.accessKey, AwsConfig.secretKey))
+                .build();
+    }
+
+    public static void main(String[] args) throws IOException {
+        AwsTranscribeVoiceService awsTranscribeVoiceService = new AwsTranscribeVoiceService();
+        awsTranscribeVoiceService.init();
+        awsTranscribeVoiceService.awsS3Utils = new AwsS3Utils();
+        awsTranscribeVoiceService.awsS3Utils.init();
+
+        awsTranscribeVoiceService.awsS3Utils.mp3UpToS3ReUrl("AwsPollyVoiceService.mp3", "./demo/AwsPollyVoiceService.mp3");
+        awsTranscribeVoiceService.transMp3ToText("AwsPollyVoiceService.mp3");
+    }
+
+    /**
+     * 翻译Mp3到文本
+     */
+    public Transcript transMp3ToText(String mp3S3KeyName) {
+        // 替换为您的转录任务名称
+        String jobName = "transmp3totext-" + UUID.randomUUID().toString().substring(0, 5);
+        String mediaS3Url = "s3://" + AwsS3Utils.bucketName + "/" + mp3S3KeyName;
+
+        StartTranscriptionJobRequest request = StartTranscriptionJobRequest.builder()
+                .transcriptionJobName(jobName)
+                .languageCode(ZH)
+                .mediaFormat(MediaFormat.MP3)
+                .media(Media.builder()
+                        .mediaFileUri(mediaS3Url)
+                        .build())
+                .build();
+
+        StartTranscriptionJobResponse response = transcribeClient.startTranscriptionJob(request);
+        String transcriptionJobName = response.transcriptionJob().transcriptionJobName();
+        log.info("Transcription job name: " + transcriptionJobName);
+
+        GetTranscriptionJobRequest getJobRequest = GetTranscriptionJobRequest.builder()
+                .transcriptionJobName(transcriptionJobName)
+                .build();
+
+        String transcriptResult = null;
+        while (true) {
+            GetTranscriptionJobResponse getJobResponse = transcribeClient.getTranscriptionJob(getJobRequest);
+            TranscriptionJob transcriptionJob = getJobResponse.transcriptionJob();
+            if (transcriptionJob.transcriptionJobStatus() == TranscriptionJobStatus.COMPLETED) {
+                transcriptResult = transcriptionJob.transcript().transcriptFileUri();
+                break;
+            } else if (transcriptionJob.transcriptionJobStatus() == TranscriptionJobStatus.FAILED) {
+                log.info("Transcription job failed");
+                break;
+            }
+            try {
+                Thread.sleep(500); // 每500ms轮询一次
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (transcriptResult != null) {
+            // 解析转录结果文件并提取转录文本
+            String transcripts = convertURLToString(transcriptResult);
+            // {"jobName":"transmp3totext-b947b","accountId":"445153820371","status":"COMPLETED","results":{"transcripts":[{"transcript":"你好"}],"items":[{"type":"pronunciation","alternatives":[{"confidence":"0.9999","content":"你好"}],"start_time":"0.0","end_time":"0.65"}]}}
+            Transcript transcript = GSON.fromJson(transcripts, Transcript.class);
+            log.info("Transcription result: " + transcript);
+            return transcript;
+        }
+
+        return null;
+    }
+
+    public String convertURLToString(String urlString) {
+        StringBuilder content = new StringBuilder();
+        try {
+            URL url = new URL(urlString);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            log.error("读取URL内容时出现错误: ", e);
+        }
+        return content.toString();
+    }
+
+    @Data
+    public static class Transcript {
+        private Result results;
+    }
+
+    @Data
+    public static class Result {
+        private List<Item> transcripts;
+    }
+
+    @Data
+    public static class Item {
+        private String transcript;
+    }
+}
--- src/main/java/group/rxcloud/ava/aigc/utils/AwsS3Utils.java
+++ src/main/java/group/rxcloud/ava/aigc/utils/AwsS3Utils.java
@@ -1,18 +1,27 @@
 package group.rxcloud.ava.aigc.utils;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
 import group.rxcloud.ava.aigc.config.AwsConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
+import java.net.URL;
+import java.util.Date;
 
+@Slf4j
 @Component
 public class AwsS3Utils {
 
-    private static String bucketName = "";
+    public static String bucketName = "aioverflow";
 
     private AmazonS3 s3client;
 
@@ -27,11 +36,62 @@ public class AwsS3Utils {
                 .build();
     }
 
-//    public String uploadFileToS3(InputStream inputStream, String fileName) {
-//
-//    }
-//
-//    public String genPresignedUrlFromS3Key(String s3Key) {
-//
-//    }s3Key
+    public static void main(String[] args) {
+        AwsS3Utils awsS3Utils = new AwsS3Utils();
+        awsS3Utils.init();
+
+        String s1 = awsS3Utils.picUpToS3ReUrl("awsrekognitionimage_output.jpg", "./demo/awsrekognitionimage_output.jpg");
+        String s2 = awsS3Utils.mp3UpToS3ReUrl("AwsPollyVoiceService.mp3", "./demo/AwsPollyVoiceService.mp3");
+        log.info("url: " + s1 + ", " + s2);
+    }
+
+    /**
+     * 将本地mp3文件上传到s3，并返回公网访问的url
+     */
+    public String mp3UpToS3ReUrl(String mp3KeyName, String path) {
+        String keyName = mp3KeyName;
+        String filePath = path;
+        try {
+            File file = new File(filePath);
+            // 设置文件的Content - Type为 "audio/mpeg"
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType("audio/mpeg");
+            PutObjectRequest request = new PutObjectRequest(bucketName, keyName, file);
+            request.setMetadata(metadata);
+            s3client.putObject(request);
+            // 获取公网访问URL
+            Date expiration = new Date(System.currentTimeMillis() + 3600000);
+            URL url = s3client.generatePresignedUrl(bucketName, keyName, expiration);
+            log.info("File uploaded to S3, URL: " + url.toString());
+            return url.toString();
+        } catch (AmazonServiceException ase) {
+            log.info("Error Message:    " + ase.getMessage());
+            log.info("HTTP Status Code: " + ase.getStatusCode());
+            log.info("AWS Error Code:   " + ase.getErrorCode());
+            log.info("Error Type:       " + ase.getErrorType());
+            log.info("Request ID:       " + ase.getRequestId());
+        } catch (Exception ace) {
+            log.info("Error Message: " + ace.getMessage());
+            throw ace;
+        }
+        return "";
+    }
+
+    public String picUpToS3ReUrl(String mp3KeyName, String path) {
+        String keyName = mp3KeyName;
+        String filePath = path;
+        try {
+            File file = new File(filePath);
+            PutObjectRequest request = new PutObjectRequest(bucketName, keyName, file);
+            PutObjectResult result = s3client.putObject(request);
+            // 获取公网可以访问的URL
+            Date expiration = new Date(System.currentTimeMillis() + 3600000);
+            URL url = s3client.generatePresignedUrl(bucketName, keyName, expiration);
+            log.info("File uploaded successfully. URL: " + url.toString());
+            return url.toString();
+        } catch (Exception ace) {
+            log.info("Error Message: " + ace.getMessage());
+            throw ace;
+        }
+    }
 }
